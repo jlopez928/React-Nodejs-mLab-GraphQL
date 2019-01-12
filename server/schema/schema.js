1017c600d8b7a77009d0cf2b72631df50ba9e2e1
Jesus Lopez <jesuslopez80@gmail.com>
2019-01-12 07:16:04 -0400

    15 - More on Root Queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -77,6 +77,18 @@ const RootQuery = new GraphQLObjectType({
                 //console.log(typeof(args.id))
                 return _.find(authors, {id:args.id})
             }
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args){
+                return books
+            }
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve(parent, args){
+                return authors
+            }
         }
     }
 })
