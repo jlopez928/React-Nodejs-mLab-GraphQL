58c6e1a078e73570776898be8a791c6ecc0bf99f
Jesus Lopez <jesuslopez80@gmail.com>
2019-01-12 07:00:47 -0400

    14 - GraphQL Lists

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,7 +6,8 @@ const {
     GraphQLString,
     GraphQLSchema,
     GraphQLID,
-    GraphQLInt    
+    GraphQLInt,
+    GraphQLList  
 } = graphql
 
 //Dummy Data
@@ -14,6 +15,9 @@ var books = [
     {name:'Name of the Wind', genre:'Fantasy',id:'1',authorid:'1'},
     {name:'The Final Empire', genre:'Fantasy',id:'2',authorid:'2'},
     {name:'The Long Earth', genre:'Sci-Fi',id:'3',authorid:'3'},
+    {name:'The Hero of Ages', genre:'Fantasy',id:'4',authorid:'2'},
+    {name:'The colour of Magic', genre:'Fantasy',id:'5',authorid:'3'},
+    {name:'The Light Fantastic', genre:'Fantasy',id:'6',authorid:'3'},
 ]
 
 var authors = [
@@ -43,7 +47,13 @@ const AuthorType = new GraphQLObjectType({
     fields: () => ({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
-        age: {type: GraphQLInt}
+        age: {type: GraphQLInt},
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args){
+                return _.filter(books, {authorid:parent.id})
+            }
+        }
     })
 })
 
