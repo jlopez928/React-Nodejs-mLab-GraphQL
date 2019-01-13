35fa2cbc6f5e2d898e72c5a292943636d56ab3a1
Jesus Lopez <jesuslopez80@gmail.com>
2019-01-13 07:16:55 -0400

    30 - Updating Component State

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -4,6 +4,15 @@ import {graphql} from 'react-apollo';
 import {getAuthorsQuery} from '../queries/queries';
 
 class AddBook extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            genre: '',
+            authorid: ''
+        }
+    }
+
     displayAuthors() {
         var data = this.props.data;
         if (data.loading) {
@@ -15,23 +24,28 @@ class AddBook extends Component {
         }
     }
 
+    submitForm(e) {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
       return (
-        <form id="add-book">
+        <form id="add-book" onSubmit={this.submitForm.bind(this)}>
         
             <div className="field">
-                <label>Book name:</label>
-                <input type="text"/>
+                <label>Book Name:</label>
+                <input type="text" onChange={(e) => this.setState({name:e.target.value})}/>
             </div>
 
             <div className="field">
                 <label>Genre:</label>
-                <input type="text"/>
+                <input type="text" onChange={(e) => this.setState({genre:e.target.value})}/>
             </div>
 
             <div className="field">
                 <label>Author:</label>
-                <select>
+                <select onChange={(e) => this.setState({authorid:e.target.value})}>
                     <option>Select author</option>
                     {this.displayAuthors()}
                 </select>
