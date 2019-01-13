ba7348538c007f8a01bb162fa618b15422dd6640
Jesus Lopez <jesuslopez80@gmail.com>
2019-01-13 10:39:14 -0400

    35 - Making a Single Query

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -7,6 +7,13 @@ import {getBooksQuery} from '../queries/queries';
 import BookDetails from './BookDetails';
 
 class BookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    }
+  }
+
   displayBooks() {
     var data = this.props.data;
 
@@ -15,7 +22,7 @@ class BookList extends Component {
     }else {
       return data.books.map(book => {
         return(
-          <li key= {book.id}>{book.name}</li>
+          <li key= {book.id} onClick={(e) => {this.setState({selected:book.id})}}>{book.name}</li>
         )
       })
     }
@@ -28,7 +35,7 @@ class BookList extends Component {
         <ul id="book-list">
            {this.displayBooks()}
         </ul>  
-        <BookDetails />  
+        <BookDetails bookId={this.state.selected}/>  
       </div>
     );
   }
